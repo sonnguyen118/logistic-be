c2ed1eb3c79ab9259af26b1900b4883bda7673cc
sonnguyen118 <sonnguyenprofessor@gmail.com>
2023-03-08 02:23:41 +0700

    authentication

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const db = require('../configs/database');
 
 const userModel = {};
@@ -17,6 +18,39 @@ userModel.getAllUsers = () => {
 userModel.getUserById = (id) => {
   return new Promise((resolve, reject) => {
     db.query('SELECT * FROM users WHERE id = ?', [id], (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (results.length === 0) {
+          resolve(null);
+        } else {
+          resolve(results[0]);
+        }
+      }
+    });
+  });
+};
+
+userModel.getUserByEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (results.length === 0) {
+          resolve(null);
+        } else {
+          resolve(results[0]);
+        }
+      }
+    });
+  });
+};
+
+userModel.createUser = (user) => {
+  const query = 'INSERT INTO `users` (`email`, `password`, `first_name`, `last_name`, `status`,`created_date`,`role`) VALUES (?,?,?,?,?,?,?)'
+  return new Promise((resolve, reject) => {
+    db.query(query, [user.email, user.password, user.firstName, user.lastName, user.status, new Date(), user.role], (err, results) => {
       if (err) {
         reject(err);
       } else {
