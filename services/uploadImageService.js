1284a4bb81084f2549e701475bdd26519cda82b7
sonnguyen118 <sonnguyenprofessor@gmail.com>
2023-03-13 22:46:28 +0700

    fix bug

--- services/uploadImageService.js
+++ services/uploadImageService.js
@@ -1,33 +1,16 @@
 const multer = require('multer');
-const dotenv = require('dotenv');
-dotenv.config();
 
-logo = process.env.LOGO_STORAGE_PATH
-avatar = process.env.AVATAR_USER_STORAGE_PATH
-article = process.env.ARTICLE_IMAGES_STORAGE_PATH
+// var uploadImages = {}
 
-// var avatarStorage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'uploads/avatar/')
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname)
-//     }
-// })
+var avatarStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/avatar/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
 
-function setUpStoragePath(path) {
-    return multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, path)
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.originalname)
-        }
-    })
-}
+const uploadImages = multer({ storage: avatarStorage })
 
-const uploadAvatar = multer({ storage: setUpStoragePath(process.env.AVATAR_USER_STORAGE_PATH) })
-const uploadLogo = multer({ storage: setUpStoragePath(process.env.LOGO_STORAGE_PATH) })
-const uploadArticle = multer({ storage: setUpStoragePath(process.env.ARTICLE_IMAGES_STORAGE_PATH) })
-
-module.exports = { uploadAvatar, uploadLogo, uploadArticle };
+module.exports = uploadImages;
